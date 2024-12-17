42f1ffca466fe6909d54035c7ecbc675befb6e2b
Francisco Antonio Mendez <fm7074978@gmail.com>
2024-12-17 12:46:46 -0400

    Actualizando dependencias

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module edu.uagrm.fich.navegador {
     requires javafx.controls;
     requires javafx.fxml;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.desktop;
+    requires javafx.web;
 
     exports edu.uagrm.fich.navegador;
 }
